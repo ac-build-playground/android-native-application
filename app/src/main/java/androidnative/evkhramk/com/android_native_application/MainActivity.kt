10af78f2ec8ca16c0f46b4e2c378a56b6462b85d
Evgenii Khramkov <evkhramk@microsoft.com>
2019-05-28 10:43:38 -0700

    init

---
+++ app/src/main/java/androidnative/evkhramk/com/android_native_application/MainActivity.kt
@@ -0,0 +1,12 @@
+package androidnative.evkhramk.com.android_native_application
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
