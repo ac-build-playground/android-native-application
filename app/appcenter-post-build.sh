ae1c95ef27e7591c587296e0a5a22315649da5dd
Evgeniy Khramkov <evkhramk@microsoft.com>
2019-05-31 08:55:32 -0700

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -3,7 +3,10 @@ BASE="$(build.sourcesdirectory)/app/build/outputs/apk"
 # $BASE is path to outputs/apk folder - for compatibility we don't change it but use relative path
 OUTPUTS=$BASE/..
 
-pushd $OUTPUTS
+# pushd $OUTPUTS
+cd $BASE
+cd ..
+
 MAPPING_TOTAL=$(find -E . -iregex ".+mapping.+mapping\.txt" | wc -l)
 if (($MAPPING_TOTAL > 0)); then
     # emit tag for when mapping files exist and will be made available
